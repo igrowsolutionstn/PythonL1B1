c820cf6db5163d4db76f19c9d3d44996a5f26b32
arnoldindia <igrowsolutionstn@gmail.com>
2022-08-06 12:32:31 +0530

    positive test case

--- Assignment01/test_hello.py
+++ Assignment01/test_hello.py
@@ -1,7 +1,8 @@
+import pytest
 import hello    # The code to test
 
 def test_increment():
     assert hello.increment(3) == 4
 
 def test_decrement():
-    assert hello.decrement(3) == 4
+    assert hello.decrement(3) == 2
