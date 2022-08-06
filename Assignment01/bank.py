b3c271aace7563f36a2a02ffac99c0db8ef3e6f6
arnoldindia <igrowsolutionstn@gmail.com>
2022-08-06 16:41:32 +0530

    reversed

--- Assignment01/bank.py
+++ Assignment01/bank.py
@@ -7,15 +7,15 @@ Direction ---> don't change any of the existing code. please add your code only 
 '''
 
 def deposit(b,a):
-    return b+a #your code here
+    return #your code here
 
 def withdraw(b,a):
-    return b-a #your code here
+    return #your code here
 
 name = "Tom"
 balance = 100
 amount = 10
-ch='w' #your code here
+ch='' #your code here
 
 if ch=='w':
     balance = withdraw(balance, amount)
@@ -24,4 +24,4 @@ elif ch=='d':
 else:
     print('Invalid Choice')
 
-print("Balance Money : ", balance) #your code here
+print("Balance Money : ") #your code here
